0295afcbe2955e04e1b02a5b3d9e1c9b61d810e2
Adik_T <adam.trzeszczak@hotmail.com>
2015-10-22 13:23:51 +0200

    app layout
    (y)

---
+++ PokeApp/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PokeApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        
+
+        private void btn1_Click(object sender, RoutedEventArgs e)
+        {
+            string userName = boxName.Text;
+            listUsers.Items.Add(new MyUserControl1(userName));
+        }
+    }
+}
---
+++ PokeApp/MyUserControl1.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PokeApp
+{
+    
+
+    public sealed partial class MyUserControl1 : UserControl
+    {
+        List<string> zapis_PokID;
+
+        public MyUserControl1(string userName)
+        {
+            
+            zapis_PokID = new List<string>();
+            this.InitializeComponent();
+            textUsrName.Text = userName;
+        }
+    }
+
+    
+}
