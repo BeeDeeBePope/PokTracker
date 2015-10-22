28f7b9db352448d4bd18d2c2e6b8049df3d107ce
Adik_T <adam.trzeszczak@hotmail.com>
2015-10-22 13:29:42 +0200

    test

--- PokeApp/MainPage.xaml.cs
+++ PokeApp/MainPage.xaml.cs
@@ -17,7 +17,7 @@ using Windows.UI.Xaml.Navigation;
 
 namespace PokeApp
 {
-    /// <summary>
+    /// <summary>///
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class MainPage : Page
